0d817bb63b71bf1e389ed89557cc0eb09fdeb4ad
kazuop <kazucheats@gmail.com>
2025-07-18 06:03:56 +0530

    Update likeCommands.py

--- cogs/likeCommands.py
+++ cogs/likeCommands.py
@@ -144,8 +144,8 @@ class LikeCommands(commands.Cog):
                     else:
                         embed.description = "\n┌MAX LIKES\n└─This UID has already received the maximum likes today.\n"
 
-                    embed.set_footer(text="DEVELOPED BY THUG")
-                    embed.description += "\n🔗 JOIN : https://discord.gg/awPm5B3QFg"
+                    embed.set_footer(text="DEVELOPED BY KAZU")
+                    embed.description += "\n🔗 JOIN : https://discord.gg/cBpBQCwmSG"
                     await ctx.send(embed=embed, mention_author=True, ephemeral=is_slash)
 
         except asyncio.TimeoutError:
